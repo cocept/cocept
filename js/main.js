7dfc71d19b4d8bdfea34485111c2278987c778ba
Max Mumford <maxmumford@gmail.com>
2014-08-14 17:32:53 +0200

    [ADD] floating navigation on scroll down [ADD] twitter bootstrap css breakpoint selectors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+
+	// sticky nav
+	$(document).scroll(function() {
+	    var scrollAmount = $(document).scrollTop();
+	    if (scrollAmount > 0){
+	    	$('nav').addClass('scrolled');
+	    	$('#content').addClass('scrolled');
+	    }
+	    else {
+	    	$('nav').removeClass('scrolled');
+	    	$('#content').removeClass('scrolled');
+	    }
+	})
+
+	// go to top button
     $('#to-top').click(function(){
         $('html, body').animate({scrollTop:0}, 'slow');
         return false;
     });
+
+
 });
